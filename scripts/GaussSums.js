6876180b6c6e0d5689bdd2e273b1f1456795771c
Shadiyah Mangru <shadiyahmangru@gmail.com>
2018-07-13 09:39:49 -0400

    GaussSums.js

--- scripts/GaussSums.js
+++ scripts/GaussSums.js
@@ -16,7 +16,7 @@ function gaussSum() {
 (function( $ ){
    $.fn.displayGaussA = function() {
         $("#GaussAlg").html("Gauss' method to easily sum the first n natural numbers:\n");
-        $("#GaussAlg").append("<strong>Step 1:</strong> Choose a number <em>n</em>.<br>" + 
+        $("#GaussAlg").append("<br><strong>Step 1:</strong> Choose a number <em>n</em>.<br>" + 
                 "<strong>Step 2:</strong> Add 1 to the number from Step 1.<br>" +
                 "<strong>Step 3:</strong> Multiply <em>n</em> by the sum from Step 2.<br>" +
                 "<strong>Step 4:</strong> Divide the product from Step 3 by 2.<br>" +
