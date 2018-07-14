640630275db217b75c9fc867699e68e2b7048df7
Shadiyah Mangru <shadiyahmangru@gmail.com>
2018-07-14 19:22:32 -0400

    FibonacciSeq.js

--- scripts/FibonacciSeq.js
+++ scripts/FibonacciSeq.js
@@ -1,5 +1,7 @@
-//Methods to calculate and return (i) the nth term of the Fibonacci sequence, and (ii) the first n terms of the Fibonacci sequence
-    
+//*************************************************************
+//code to create Fibonacci Sequence Calculator Tab
+//*************************************************************
+    //calc nth term of Fibonacci sequence
     function fibCalc(Num){
         var fibArray = [];
         Num = parseInt(Num);
@@ -24,11 +26,12 @@
             return fibNew;
         }
     }
-    
+    //return nth term of Fibonacci sequence
     function nthFibCalc(Num){
         document.querySelector('#nthTerm').innerHTML = "The Fibonacci Sequence term at position " + Num + " is: " + fibCalc(parseInt(Num));
     }
     
+    //calculate and return the first n terms of the Fibonacci sequence
     function nFibCalc(Num){
         var firstN =[];
         for(var i = 1; i<parseInt(Num)+1; i++){
@@ -37,56 +40,34 @@
         document.querySelector("#nTerms").innerHTML = "The first " + Num + " terms of the Fibonacci sequence are: " + firstN.join(", ");
     }
    
-//******************************************
-//display intro to Fibonacci Sequence in tab
-//******************************************
-(function( $ ){
-   $.fn.displayFibIntro = function() {
-        $("#fibIntro").html("The Fibonacci Sequence is: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, .... \nEach number in the sequence (starting from the third term) is the sum of the two preceding numbers.");
-       $("#fibIntro").append("<br><br>");
-   }; 
-})( jQuery );
  
 $(document).ready(function(){
-    $('#fibIntro').displayFibIntro();
-});
-
-//************************************************
-//display Fibonacci nth term Calculator in sub tab
-//************************************************
-(function( $ ){
-   $.fn.displayFibNthCalc = function() {
-        $("#fibNthCalc").html("Find the nth term of the Fibonacci Sequence");
+    //display intro to Fibonacci Sequence in tab
+    $("#fibIntro").html("The Fibonacci Sequence is: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, .... \nEach number in the sequence (starting from the third term) is the sum of the two preceding numbers.");
+    $("#fibIntro").append("<br><br>");
+    
+    
+    //display Fibonacci nth term Calculator in sub tab
+    $("#fibNthCalc").html("Find the nth term of the Fibonacci Sequence");
         $("#fibNthCalc").append("<br><br><label class='siteLabel'>Enter n value:</label> <input class='siteInput' id='nth' type='text' value=''>");
         $("#fibNthCalc").append("<button class='siteButton' id='fB'>Get nth term</button><br>" +
         "<div id='nthTerm' class='fibO'></div><br><br>");
-   }; 
-})( jQuery );
- 
-$(document).ready(function(){
-    $('#fibNthCalc').displayFibNthCalc();
+    
+    //press button to get nth term
     $( "#fB" ).click(function() {
        nthFibCalc($('#nth').val()); 
     });
-}); 
-    
+  
+
+    //display Fibonacci Sequence Calculator in sub tab
+    $("#fibSeqPrint").html("Print the first n terms of the Fibonacci Sequence");
+    $("#fibSeqPrint").append("<br><br><label class='siteLabel'>Enter n value:</label> <input class='siteInput' id='nVal' type='text' value=''>" +
+    "<button class='siteButton' id='fBp'>Get first n terms</button><br>" +
+    "<div id='nTerms' class='fibO'></div><br><br>");
  
- 
-//************************************************
-//display Fibonacci Sequence Calculator in sub tab
-//************************************************
-(function( $ ){
-   $.fn.displayFibSeqPrint = function() {
-        $("#fibSeqPrint").html("Print the first n terms of the Fibonacci Sequence");
-        $("#fibSeqPrint").append("<br><br><label class='siteLabel'>Enter n value:</label> <input class='siteInput' id='nVal' type='text' value=''>" +
-        "<button class='siteButton' id='fBp'>Get first n terms</button><br>" +
-        "<div id='nTerms' class='fibO'></div><br><br>");
-   }; 
-})( jQuery );
- 
-$(document).ready(function(){
-    $('#fibSeqPrint').displayFibSeqPrint();
+    //press button to get first n terms
     $( "#fBp" ).click(function() {
        nFibCalc($('#nVal').val());
     });
-}); 
+});   
+    
