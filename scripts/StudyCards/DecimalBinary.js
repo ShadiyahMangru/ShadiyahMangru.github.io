36a52fba200ebc1bc6a648d5d7c7d60dccb3db3c
Shadiyah Mangru <shadiyahmangru@gmail.com>
2018-07-19 16:31:08 -0400

    DecimalBinary.js

--- scripts/StudyCards/DecimalBinary.js
+++ scripts/StudyCards/DecimalBinary.js
@@ -4,12 +4,14 @@
 
 $("#bToDPractice").append("<span class='highlight'>Binary to Decimal Conversions Practice</span><br><label>Enter a Binary Number: </label><input type='number' id='bin' class='siteInput' placeholder='Binary'><br><label>Enter the Decimal Equivalent: </label><input type='number' id='bToDResult' class='siteInput'><button class='siteButton' id='bToDConvert'>Correct?</button><br><div id='isBToDCorrect'></div>"); 
 
-var addend;
-var twoPower;
-var decimalSum=0;
-var binaryDigitsArray = new Array();
+
+
 //function to convert binary input to decimal output
 $("#bToDConvert").click(function() {
+    var binaryDigitsArray = new Array();
+    var addend;
+    var twoPower;
+    var decimalSum=0;
     var Num = $("#bin").val();
     binaryDigitsArray = Num.split("");
     for(i=0; i < binaryDigitsArray.length; i++){
@@ -28,7 +30,6 @@ $("#bToDConvert").click(function() {
    
   binaryDigitsArray = []; //empty array
 });
-
 
 var quotient;
 var remainder;
@@ -59,6 +60,9 @@ function decToBinary(Num){
         $("#isDToBCorrect").html("Incorrect.  Try Again!");
     }
     remaindersArray = []; //empty array
+    counter=0;
+    remainder=0;
+    quotient=0;
     }
 } 
 
@@ -66,9 +70,9 @@ function decToBinary(Num){
 $(document).ready(function(){
   
     
-$("#BinDecOptions").append("Select Study Option:<br><button class='siteButton' id='binToDecPracticeShow'>Binary to Decimal Conversions Practice</button><button class='siteButton' id='decToBinPracticeShow'>Decimal to Binary Conversions Practice</button><button class='siteButton' id='binToDecShow'>Review Binary to Decimal Conversion</button><button class='siteButton' id='decToBinShow'>Review Decimal to Binary Conversion</button><br><br>");    
+$("#BinDecOptions").append("Select Study Option:<br><button id='binToDecPracticeShow' class='dispOpt'>Binary to Decimal Conversions Practice</button><button class='dispOpt' id='decToBinPracticeShow'>Decimal to Binary Conversions Practice</button><button class='dispOpt' id='binToDecShow'>Review Binary to Decimal Conversion</button><button class='dispOpt' id='decToBinShow'>Review Decimal to Binary Conversion</button<br><br>");    
     
-$("#binToDec").append("EXAMPLE: Convert the <span class='highlight'>Binary</span> number 1101101 to its Decimal equivalent.<div id='block'>1101101 = (1)*2<sup>6</sup> + (1)*2<sup>5</sup> + (0)*2<sup>4</sup> + (1)*2<sup>3</sup> + (1)*2<sup>2</sup> + (0)*2<sup>1</sup> + (1)*2<sup>0</sup>= 64 + 32 + 0 + 8 + 4 + 0 + 1=  109<br>Therefore<div class='row'><div class='column'><table class='BDtable'><caption>BINARY</caption><thead><tr><th scope='col'></th><th scope='col'>2<sup>6</sup></th><th scope='col'>2<sup>5</sup></th><th scope='col'>2<sup>4</sup></th><th scope='col'>2<sup>3</sup></th><th scope='col'>2<sup>2</sup></th><th scope='col'>2<sup>1</sup></th><th scope='col'>2<sup>0</sup></th></tr></thead><tbody><tr><th scope='row'>Bit</th><td>1</td><td>1</td><td>0</td><td>1</td><td>1</td><td>0</td><td>1</td></tr></tbody></table></div><div class='column'><h3>=</h3></div><div class='column'><table class='BDtable'><caption>DECIMAL</caption><thead><th scope='column'></th><th>10<sup>2</sup></th><th>10<sup>1</sup></th><th>10<sup>0</sup></th></thead><tbody><tr><th scope='row'>Digit</th><td>1</td><td>0</td><td>9</td></tr></tbody></table></div></div></div><br><br>GENERALIZED STEPS TO SOLVE:<ol><li>Multiply each bit of the binary number by 2 to the appropriate power and sum these products.</li><li>The final sum is the converted decimal value.</li></ol>");    
+$("#binToDec").append("EXAMPLE: Convert the <span class='highlight'>Binary</span> number 1101101 to its Decimal equivalent.<br><br>Remember<br><table class='BDtable'><caption>BINARY</caption><thead><tr><th scope='col'></th><th scope='col'>2<sup>6</sup></th><th scope='col'>2<sup>5</sup></th><th scope='col'>2<sup>4</sup></th><th scope='col'>2<sup>3</sup></th><th scope='col'>2<sup>2</sup></th><th scope='col'>2<sup>1</sup></th><th scope='col'>2<sup>0</sup></th></tr></thead><tbody><tr><th scope='row'>Bit</th><td>1</td><td>1</td><td>0</td><td>1</td><td>1</td><td>0</td><td>1</td></tr></tbody></table><br>Therefore<br><div id='block'>1101101 = (1)*2<sup>6</sup> + (1)*2<sup>5</sup> + (0)*2<sup>4</sup> + (1)*2<sup>3</sup> + (1)*2<sup>2</sup> + (0)*2<sup>1</sup> + (1)*2<sup>0</sup>= 64 + 32 + 0 + 8 + 4 + 0 + 1=  109<br></div><br><br>GENERALIZED STEPS TO SOLVE:<ol><li>Multiply each bit of the binary number by 2 to the appropriate power and sum these products.</li><li>The final sum is the converted decimal value.</li></ol>");    
     
     
 $("#decToBin").append("EXAMPLE: Convert the <span class='highlight'>Decimal</span> number 100 to its Binary equivalent.<br>100 &#247 2 = 50 R0<br>50 &#247 2 = 25 R0<br>25 &#247 2 = 12 R1<br>12 &#247 2 = 6 R0<br>6 &#247 2 = 3 R0<br>3 &#247 2 = 1 R1<br>1 &#247 2 = 0 R1<br>Therefore 100 (decimal) = 1100100 (binary)<br><br>GENERALIZED STEPS TO SOLVE:<br><ol><li>Given a number, <em>n</em>, divide by 2 and record the quotient, <em>q</em>, and the remainder, <em>r</em> (<em>r</em> will equal 1 or 0).</li><li>While <em>q</em> &#8805 1, divide <em>q</em> by 2 and record the new <em>q</em> and the new <em>r</em>.</li><li>Listing the remainders (in order from last one found to first one found) gives the binary equivalent to the original decimal number.</li></ol>");     
