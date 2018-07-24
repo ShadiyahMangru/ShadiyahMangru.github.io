6fdd71ee91fc29abdca8cb0fa60c1d4a5b6fe38a
Shadiyah Mangru <shadiyahmangru@gmail.com>
2018-07-24 19:23:51 -0400

    Update AddItUp.js

--- scripts/Games/AddItUp.js
+++ scripts/Games/AddItUp.js
@@ -11,29 +11,29 @@
     var aSolution = ["13425", "42351", "25143", "51234", "34512"]; 
         
     function showKey(){
-        var rbyg = ["10", "27", "16", "22"];
-        var key = "<span style='color: #003399; font-size: 120%; font-weight: 600'><u>KEY:</u></span> \n\nThe sum of: (i) the <span style='color:#DD0040'>red</span> squares is " + rbyg[0] + ", (ii) the <span style='color: #1674B1'>blue</span> squares is " + rbyg[1] + ", (iii) the <span style='color: #F9AB13'>yellow</span> squares is " + rbyg[2] + ", (iv) the <span style='color: #53904E'>green</span> squares is " + rbyg[3]; 
+        var rbpg = ["10", "27", "16", "22"];
+        var key = "<span style='color: #003399; font-size: 120%; font-weight: 600'><u>KEY:</u></span> \n\nThe sum of: (i) the <span style='color:#ff0000'>red</span> squares is " + rbpg[0] + ", (ii) the <span style='color: #006bff'>blue</span> squares is " + rbpg[1] + ", (iii) the <span style='color: #b825df'>purple</span> squares is " + rbpg[2] + ", (iv) the <span style='color: #2c8309'>green</span> squares is " + rbpg[3]; 
         $("#key").html(key);
         $("#key").append("<br><br>")
     }
         
     function createGameBoard(){
-    var answerKeyC = ["rrbbb", "rrbbb", "yyybb", "yyygg", "ggggg"];
+    var answerKeyC = ["rrbbb", "rrbbb", "pppbb", "pppgg", "ggggg"];
     var counter = 0;
     var setStyle;
         while(counter < 5){
             for(var i = 0; i < 5; i++){
                 if(answerKeyC[counter].charAt(i)==='r'){
-                    setStyle = "style='background-color: #DD0040'";
+                    setStyle = "style='background-color: #ff0000'";
                 }
                 else if(answerKeyC[counter].charAt(i)==='b'){
-                    setStyle = "style='background-color: #1674B1'";
+                    setStyle = "style='background-color: #006bff'";
                 }
-                else if(answerKeyC[counter].charAt(i)==='y'){
-                    setStyle = "style='background-color: #F9AB13'";
+                else if(answerKeyC[counter].charAt(i)==='p'){
+                    setStyle = "style='background-color: #B825DF'";
                 }    
                 else if(answerKeyC[counter].charAt(i)==='g'){
-                    setStyle = "style='background-color: #53904E'";
+                    setStyle = "style='background-color: #2c8309'";
                 }     
                 $("#gb").append('<button ' + setStyle + 'class="clickButton" id="a' + i + counter + '">'+ 1 +'</button>');
             }
