9a1d29c24ea79dbe080aca150c848fc6724f2ef2
Shadiyah Mangru <shadiyahmangru@gmail.com>
2018-08-01 13:13:57 -0400

    UTube.js

---
+++ scripts/UTube.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function(){
+$("#clear").click(function(){
+    $("#screen").html("");
+});     
+
+$("#Weezer").click(function(){
+    $("#screen").html("");
+    $("#screen").append("<p>My name is Jonas<br>I'm carrying the wheel<br>Thanks for all you've shown us<br>But this is how we feel<br>Come sit next to me<br>Pour yourself some tea<br>Just like Grandma made<br>When we couldn't find sleep<br>Things were better then<br>Once but never again<br>We've all left the den<br>Let me tell you about it...<br></p>");
+});    
+    
+$("#PJHarvey").click(function(){
+    $("#screen").html("");
+    $("#screen").append("<p>On a rooftop in Brooklyn<br>At one in the morning<br>Watching the lights flash<br>In Manhattan<br>I see five bridges<br>The Empire State Building<br> And you said something<br>That I've never forgotten<br>...<br></p>");
+});
+
+$("#TalkingHeads").click(function(){
+    $("#screen").html("");
+    $("#screen").append("<p>And you may find yourself<br>Living in a shotgun shack<br>And you may find yourself<br>In another part of the world<br>And you may find yourself<br>Behind the wheel of a large automobile<br>And you may find yourself in a beautiful house<br>With a beautiful wife<br>And you may ask yourself, well<br>How did I get here?...<br></p>");
+});
+
+$("#PassionPit").click(function(){
+    $("#screen").html("");
+    $("#screen").append("<p>Since my heart is golden<br>I've got sense to hold in<br>Tempted just to make an ugly scene<br>No I'm not as proper<br>My money's in copper<br>Ripped down from the brownstones to the street<br>Listen, I'm your friend, don't quote me<br>But not a friend worth noting<br>Yes, please don't ever note me as your friend<br>Who says we have cold hearts?<br>Acting out our old parts<br>Let's perform our favorite little scene, oh oh oh oh...<br></p>");
+});
+    
+$("#TheShins").click(function(){
+    $("#screen").html("");
+    $("#screen").append("<p>One wound up punch of intuition<br>Lays flat my whole take on us.<br>You're the girl on the wing of a barnstormer<br>The tidal rabbit who came of age before her time.<br>We could have been so good-natured<br>If I'd relented when you insisted,<br>But we've been backed against<br>All nature's walls far too long.<br>You felt abandoned by me,<br>I recall the sunshine as you were melting<br> And though the comedy softens the fall<br>They still hear us with their ears to the wall....<br></p>");
+});
+    
+$("#NewOrder").click(function(){
+    $("#screen").html("");
+    $("#screen").append("<p>Every time I think of you<br>I feel shot right through with a bolt of blue<br> It's no problem of mine<br>But it's a problem I find<br>Living a life that I can't leave behind<br>But there's no sense in telling me<br>The wisdom of the fool won't set you free<br>But that's the way that it goes<br>And it's what nobody knows<br>Well every day my confusion grows...<br></p>");
+});
+
+});    
