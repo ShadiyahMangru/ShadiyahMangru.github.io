f263adda95a2fe5cd63e9baf121ffedc3c67d4df
Shadiyah Mangru <shadiyahmangru@gmail.com>
2018-09-02 10:40:26 -0400

    Add files via upload

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -1,19 +1,14 @@
 var main=angular.module('main',[]);
 
 main.controller("mainCtrl", function ($scope){ 
-   
-    $scope.descrip = {
-       header : "APP-ly Your Quantitative and Verbal Skills",
-       codeWord : "Accept your secret mission by strategically entering vowels during Code Word",
-       sumPuzzles : "Avert disaster by arranging the numbers in a winning combination during Sum Puzzles",
-       numberGuess : "Thwart villainous plots by strategically bisecting numeric intervals during Number Guess",
-       war : "Achieve victory with correct quantitative comparisons during War"
-   }
-     
-   $scope.pic = {
-       codeWord : "Images/usmc1.JPG",
-       sumPuzzles : "Images/usmc2.JPG",
-       numberGuess : "Images/usmc3.JPG",
-       war : "Images/usmc4.JPG"
-   }    
+    
+    $scope.header = "APP-ly Your Quantitative and Verbal Skills";
+    
+    var codeWord = {descrip: "Accept your secret mission by strategically entering vowels during Code Word", pic: "Images/usmc1.JPG"};
+    var sumPuzzles = {descrip: "Avert disaster by arranging the numbers in a winning combination during Sum Puzzles", pic: "Images/usmc2.JPG"};
+    var numberGuess = {descrip: "Thwart villainous plots by strategically bisecting numeric intervals during Number Guess", pic: "Images/usmc3.JPG"};
+    var war = {descrip: "Achieve victory with correct quantitative comparisons during War", pic: "Images/usmc4.JPG"};
+    
+    $scope.descripPic = [codeWord, sumPuzzles, numberGuess, war];
+       
 });
