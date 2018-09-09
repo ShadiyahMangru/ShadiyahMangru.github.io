c3c13326778b9cf1b8e9f93941b2a418ac5c8402
Shadiyah Mangru <shadiyahmangru@gmail.com>
2018-09-09 09:45:15 -0400

    Add files via upload

---
+++ controllers/Memory.js
@@ -0,0 +1,14 @@
+var memory = angular.module("memory", []);
+memory.controller("MemoryCtrl", function ($scope) {
+    $scope.mainTitle = "Memory";
+    $scope.contentsHeader = "Advance to your final mission by using your short-term memory skills and mental agility to identify all of the matching image pairs in the alloted time. (In Progress)";
+    
+    var pic1 = {image: "Images/usmcM1.JPG", id: 1};
+    var pic2 = {image: "Images/usmcM2.JPG", id: 2};
+    var pic3 = {image: "Images/usmcM3.JPG", id: 3};
+    var pic4 = {image: "Images/usmcM4.JPG", id: 4};
+    var pic5 = {image: "Images/usmcM5.JPG", id: 5};
+    var pic6 = {image: "Images/usmcM6.JPG", id: 6};
+    
+    $scope.picObjArray = [pic1, pic2, pic6, pic4, pic3, pic5, pic1, pic3, pic4, pic5, pic6, pic2];   
+});
--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -4,11 +4,20 @@ main.controller("mainCtrl", function ($scope){
     
     $scope.header = "Problem Solving: 'Improvise, Adapt and Overcome'";
     
-    var codeWord = {descrip: "Accept your secret mission by strategically entering vowels during Code Word.", pic: "Images/usmc1.JPG", caption: "'NEW YORK, N.Y. (May 26, 2018) - Staff Sgt. Raymond Brown, with Special Purpose Marine Air-Ground Task Force Fleet Week New York, watches the Manhattan Bridge and New York pass below on a flight to Marine Day...in Brooklyn, New York.... Fleet Week is the city's time-honored celebration of the sea services. It is an unparalleled opportunity for the citizens of New York and the surrounding tri-state area to...witness firsthand the latest capabilities of today's maritime services.' --Marines.mil "};
-    var sumPuzzles = {descrip: "Avert disaster by arranging numbers in a winning combination during Sum Puzzles.", pic: "Images/usmc2.JPG", caption: "'U.S. FIFTH FLEET AREA OF OPERATIONS (Dec. 22, 2017) - A Sailor with USS San Diego observes USNS Washington Chambers during a replenishment at sea. San Diego, with the embarked 15th Marine Expeditionary Unit, is deployed to the U.S. 5th Fleet area of operations in support of maritime security operations to reassure allies and partners and preserve the freedom of navigation and the free flow of commerce in the region. ' -- Marines.mil"};
-    var numberGuess = {descrip: "Foil villainous plots by strategically bisecting numeric intervals during Number Guess.", pic: "Images/usmc3.JPG", caption: "'Naval Air Station Sigonella, Italy (Nov. 17, 2017) - An Explosive Ordnance Disposal technician assigned to Special Purpose Marine Air-Ground Task Force-Crisis Response-Africa controls a bomb disposal robot during non-combatant evacuation operation training aboard Naval Air Station Sigonella, Italy.... SPMAGTF-CR-AF deployed to conduct limited crisis-response and theater-security operation in Europe and Africa. ' --Marines.mil"};
-    var war = {descrip: "Achieve victory with correct quantitative comparisons during War.", pic: "Images/usmc4.JPG", caption: "'FORT A.P. HILL, Va. (July 31, 2018) - A Marine with Battalion Landing Team, 1st Battalion, 2nd Marines, 22nd Marine Expeditionary Unit fast ropes out of an MV-22 Osprey during the 22nd MEU Deployment for Training at Fort A.P. Hill, VA.... In preparation for their upcoming deployment, the Marines practiced embassy reinforcement by fast roping onto a rooftop and clearing out a simulated embassy building....' --Marines.mil "};
+    var codeWord = "Accept your secret mission by strategically entering vowels during Code Word.";
+    var sumPuzzles = "Avert disaster by arranging numbers in a winning combination during Sum Puzzles.";
+    var numberGuess = "Foil villainous plots by strategically bisecting numeric intervals during Number Guess.";
+    var memory = "Approach mission completion through a display of mental agility during Memory.";
+    var war = "Achieve victory with correct quantitative comparisons during War.";
     
-    $scope.descripPic = [codeWord, sumPuzzles, numberGuess, war];
-       
+    var marines1 = {pic: "Images/usmc1.JPG", caption: "'NEW YORK, N.Y. (May 26, 2018) - Staff Sgt. Raymond Brown, with Special Purpose Marine Air-Ground Task Force Fleet Week New York, watches the Manhattan Bridge and New York pass below on a flight to Marine Day...in Brooklyn, New York.... Fleet Week is the city's time-honored celebration of the sea services. It is an unparalleled opportunity for the citizens of New York and the surrounding tri-state area to...witness firsthand the latest capabilities of today's maritime services.' --Marines.mil "};
+    
+    var marines2 = {pic: "Images/usmc2.JPG", caption: "'U.S. FIFTH FLEET AREA OF OPERATIONS (Dec. 22, 2017) - A Sailor with USS San Diego observes USNS Washington Chambers during a replenishment at sea. San Diego, with the embarked 15th Marine Expeditionary Unit, is deployed to the U.S. 5th Fleet area of operations in support of maritime security operations to reassure allies and partners and preserve the freedom of navigation and the free flow of commerce in the region. ' -- Marines.mil"};
+    
+    var marines3 = {pic: "Images/usmc3.JPG", caption: "'Naval Air Station Sigonella, Italy (Nov. 17, 2017) - An Explosive Ordnance Disposal technician assigned to Special Purpose Marine Air-Ground Task Force-Crisis Response-Africa controls a bomb disposal robot during non-combatant evacuation operation training aboard Naval Air Station Sigonella, Italy.... SPMAGTF-CR-AF deployed to conduct limited crisis-response and theater-security operation in Europe and Africa. ' --Marines.mil"};
+    
+    var marines4 = {pic: "Images/usmc4.JPG", caption: "'FORT A.P. HILL, Va. (July 31, 2018) - A Marine with Battalion Landing Team, 1st Battalion, 2nd Marines, 22nd Marine Expeditionary Unit fast ropes out of an MV-22 Osprey during the 22nd MEU Deployment for Training at Fort A.P. Hill, VA.... In preparation for their upcoming deployment, the Marines practiced embassy reinforcement by fast roping onto a rooftop and clearing out a simulated embassy building....' --Marines.mil "};
+    
+    $scope.descrip = [codeWord, sumPuzzles, numberGuess, memory, war];
+    $scope.marinesPics = [marines1, marines2, marines3, marines4];
 });
