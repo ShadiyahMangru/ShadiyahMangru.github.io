a5d43e00f5626725947c6b5bbe7c70172e9667fc
Shadiyah Mangru <shadiyahmangru@gmail.com>
2018-09-11 13:28:45 -0400

    Add files via upload

--- controllers/NumberGuess.js
+++ controllers/NumberGuess.js
@@ -1,6 +1,5 @@
 var numberGuess = angular.module("numberGuess", []);
 numberGuess.controller("NumberGuessCtrl", function ($scope) {
-    $scope.mainTitle = "Number Guess";
     $scope.contentsHeader = "Thwart villain Simon's diabolical scheme by besting him at his own game.  Simon says: 'the computer is thinking of an integer between 1 and 100, inclusive.  Prevent explosives detonation by identifying the number in 10 guesses or less.'";
  
 //function to generate a random integer between two values
@@ -10,52 +9,38 @@ $scope.getRandomInt = function(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min; //max exclusive, min inclusive
 };    
     
-$scope.computerNumber = parseInt($scope.getRandomInt(1, 101));
-$scope.remGuess = 10;
+$scope.rNumber = parseInt($scope.getRandomInt(1, 101));
+$scope.playerGuess;        
 $scope.alreadyG = [];
 $scope.ngFeedback;
 $scope.disableSubmitG = false;
-$scope.playerGuess;
+
         
 //checks if guess is correct and provides user with feedback    
 $scope.submitNG = function (){
     $scope.playerGuess = parseInt($scope.playerGuess);
-    //check to see if entry is a repeat
     if($scope.alreadyG.indexOf($scope.playerGuess)!=-1){
-        $scope.ngFeedback = "You have already guessed " + $scope.playerGuess + ".  You still have " + $scope.remGuess + " guesses remaining.  Try Again!"; 
-        $scope.playerGuess = "";    
+        $scope.ngFeedback = "You have already guessed " + $scope.playerGuess + ".  You still have " + (10-$scope.alreadyG.length) + " guesses remaining.  Try Again!";    
     }
     else{
     $scope.alreadyG.unshift($scope.playerGuess);
-	$scope.remGuess--;
-    var win=false;
-        if($scope.playerGuess===$scope.computerNumber){
-            $scope.ngFeedback = "Bomb successfully disarmed! The number was " + $scope.computerNumber + " and it only took you " + (10-$scope.remGuess) + " tries!";
-            $scope.playerGuess = "";
+        if($scope.playerGuess===$scope.rNumber){
+            $scope.ngFeedback = "Bomb successfully disarmed! The number was " + $scope.rNumber + " and it only took you " + $scope.alreadyG.length + " tries!";
             $scope.disableSubmitG = true;
-            win=true;
         }
-        else if($scope.playerGuess !== $scope.computerNumber && $scope.remGuess > 0){
-            if($scope.playerGuess > $scope.computerNumber){
-                if($scope.remGuess===10){
-                    $scope.remGuess--;
-                }
-            $scope.ngFeedback = "LOWER than " + $scope.playerGuess + "..." + $scope.remGuess + " guesses remaining!"; 
-            $scope.playerGuess = "";
+        else if($scope.playerGuess !== $scope.rNumber && $scope.alreadyG.length < 10){
+            if($scope.playerGuess > $scope.rNumber){
+            $scope.ngFeedback = "LOWER than " + $scope.playerGuess + " ..." + (10-$scope.alreadyG.length) + " guesses remaining!"; 
             }
-            else if($scope.playerGuess < $scope.computerNumber){
-                if($scope.remGuess===10){
-                    $scope.remGuess--;
-                }
-                $scope.ngFeedback = "HIGHER than " + $scope.playerGuess + "..." + $scope.remGuess + " guesses remaining!";
-                $scope.playerGuess = "";
+            else{
+                $scope.ngFeedback = "HIGHER than " + $scope.playerGuess + " ..." + (10-$scope.alreadyG.length) + " guesses remaining!";
             }
         }
-        else if($scope.remGuess===0 && win===false){
-            $scope.ngFeedback = "Disaster!  You are out of guesses. The correct number was " + $scope.computerNumber + ".";
-            $scope.playerGuess = "";
+        else if($scope.alreadyG.length===10){
+            $scope.ngFeedback = "Disaster!  You are out of guesses. The correct number was " + $scope.rNumber + ".";
             $scope.disableSubmitG = true;
         }
     }
+    $scope.playerGuess = "";
 };       
 });
