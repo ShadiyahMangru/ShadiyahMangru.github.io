beee0f3812ba47afdd77d7f74f5cfee63b4b7bf6
Shadiyah Mangru <shadiyahmangru@gmail.com>
2018-09-13 10:46:32 -0400

    Add files via upload

--- controllers/BaseChange.js
+++ controllers/BaseChange.js
@@ -3,20 +3,20 @@ baseChange.controller("BaseChangeCtrl", function ($scope) {
     $scope.contentsHeader = "Before being stationed overseas, you must correctly perform ten conversions between base 2 (binary) and base 10 (decimal).  (In Progress)"; 
  
 //function to generate a random integer between two values
-$scope.getRandomInt = function(min, max) {
+function getRandomInt(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min)) + min; //max exclusive, min inclusive
 };     
         
-$scope.binDecConvert = function(num, origBase, newBase) {
+function binDecConvert(num, origBase, newBase) {
     if (origBase === 10){
         return (parseInt(num)).toString(newBase)
     }
     else {
         return parseInt(num, origBase);
     }
-}
+};
 
 $scope.output;
 $scope.converted;
@@ -27,9 +27,9 @@ $scope.feedback;
 $scope.type;
 
 $scope.getNum = function(){
-    var rNumber = $scope.getRandomInt(1, 51);
+    var rNumber = getRandomInt(1, 51);
     if($scope.attempts%2===0){
-        $scope.output = $scope.binDecConvert(rNumber, 10, 2);
+        $scope.output = binDecConvert(rNumber, 10, 2);
         $scope.type = "Decimal";
     }
     else{
@@ -43,29 +43,24 @@ $scope.getNum();
 $scope.isCorrect = function(){
 $scope.attempts++;
     if($scope.type==="Binary"){
-        $scope.compare = $scope.binDecConvert($scope.output, 10, 2);
-        if($scope.userEquiv == $scope.compare){
-            $scope.feedback = "correct";
-            $scope.correct++;
-        }
-        
-        else{
-        $scope.feedback = "incorrect";
-        }
+        $scope.compare = binDecConvert($scope.output, 10, 2);
+        getFeedback();
     }
     else{
-        $scope.compare = $scope.binDecConvert($scope.output, 2, 10);
-        if($scope.userEquiv == $scope.compare){
-            $scope.feedback = "correct";
-            $scope.correct++;
-        }
-        
-        else{
-        $scope.feedback = "incorrect";
-        }
+        $scope.compare = binDecConvert($scope.output, 2, 10);
+        getFeedback();
     }
     $scope.userEquiv="";
     $scope.getNum();
 };
-    
+ 
+function getFeedback(){
+    if($scope.userEquiv == $scope.compare){
+        $scope.feedback = "correct";
+        $scope.correct++;
+    }    
+    else{
+        $scope.feedback = "incorrect";
+    }
+};
 });
