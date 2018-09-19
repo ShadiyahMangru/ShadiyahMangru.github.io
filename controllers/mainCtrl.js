ab43291bcca9fa013875548bc7a4210d79cdb593
Shadiyah Mangru <shadiyahmangru@gmail.com>
2018-09-19 15:16:43 -0400

    Add files via upload

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -11,14 +11,5 @@ main.controller("mainCtrl", function ($scope){
     var memory = "employing a strategic process of elimination to maximize matches while minimizing attempts during Memory";
     var war = "consistently making accurate quantitative comparisons during Points Faceoff";
     
-    var marines1 = {pic: "Images/usmc1.JPG", caption: ""};
-    
-    var marines2 = {pic: "Images/usmc2.JPG", caption: ""};
-    
-    var marines3 = {pic: "Images/usmc3.JPG", caption: ""};
-    
-    var marines4 = {pic: "Images/usmc4.JPG", caption: ""};
-    
     $scope.descrip = [codeWord, baseChange, sumPuzzles, numberGuess, memory, war];
-    $scope.marinesPics = [marines1, marines2, marines3, marines4];
 });
