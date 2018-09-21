0a062ff614296c179191db65957549923eb33d14
Shadiyah Mangru <shadiyahmangru@gmail.com>
2018-09-21 11:53:24 -0400

    Add files via upload

--- controllers/Stats.js
+++ controllers/Stats.js
@@ -1,97 +1,104 @@
 var stats = angular.module("stats", []);
 stats.controller("StatsCtrl", function ($scope) {
     $scope.contentsHeader = "Fill-in the 2018 Stanley Cup Champions' missing stats.";
-     
-function getGoals(shots, shootingP){
-    var goals = (shootingP/100) * shots;
-    goals = parseFloat(goals.toPrecision(4));
-    return Math.ceil(goals);
-};
-    
-function getShots(goals, shootingP){
-    if(shootingP===0){
-        return 0;
-    }
-    else{
-        var shots = (100*goals) / shootingP;
-        shots = parseFloat(shots.toPrecision(4));
-        return Math.ceil(shots);
-    }
-};
-    
-function getShootingP(goals, shots){
-    if(shots===0){
-        return 0.000;
-    }
-    else{
-        var shootingP = (goals / shots)*100;
-        shootingP = parseFloat(shootingP.toPrecision(4));
-        return shootingP;
-    }
-};
 
-$scope.playerArray = stats2018PlayoffsArray;   
-
-$scope.attempts = 0;
-$scope.correct = 0;
-$scope.userEquiv; 
+//$scope.attempts = 0;
+//$scope.correct = 0;
+ 
+$scope.userInput; 
 $scope.feedback;
 $scope.disableCorrect = false;
 $scope.unknownVar;
 $scope.player;
-$scope.missingVar;
+$scope.missingVar;    
+$scope.temp;    
     
-$scope.loadScenario = function(){
-    $scope.player = randArrayE($scope.playerArray);
-    $scope.unknownVar = getRandomInt(0,3);
-    if($scope.unknownVar === 0){
-        $scope.player.goals = "____";
-        $scope.missingVar = "total goals scored";
+    //selects a random player from the 2018 Playoffs roster
+    //randomly selects goals/shots/shootingPercentage for the user to calculate
+    function loadScenario(){
+        $scope.disableCorrect = false;
+        $scope.player = randArrayE(stats2018PlayoffsArray);
+        $scope.unknownVar = getRandomInt(0,3);
+        if($scope.unknownVar === 0){
+            $scope.temp = $scope.player.goals;
+            $scope.player.goals = "____";
+            $scope.missingVar = "total goals scored";
+        }
+        else if($scope.unknownVar === 1){
+            $scope.temp = $scope.player.shots;
+            $scope.player.shots = "____";
+            $scope.missingVar = "shots on goal";
+        }
+        else{
+            $scope.player.shootingPercentage = "____";
+            $scope.missingVar = "shooting percentage";
+        }
     }
-    else if($scope.unknownVar === 1){
-        $scope.player.shots = "____";
-        $scope.missingVar = "shots on goal";
-    }
-    else{
-        $scope.player.shootingPercentage = "____";
-        $scope.missingVar = "shooting percentage";
-    }
-}    
+    
+    //input: shots, shooting percentage
+    //output: goals
+    function getGoals(shots, shootingP){
+        var goals = (shootingP/100) * shots;
+        goals = parseFloat(goals.toPrecision(4));
+        return Math.ceil(goals);
+    };
 
-$scope.loadScenario();
- 
-$scope.isCorrect = function(){
-$scope.attempts++;
-    if($scope.unknownVar===0){
-        var g = getGoals($scope.player.shots, $scope.player.shootingPercentage);
-        if($scope.userEquiv===g){
-            $scope.feedback = "CORRECT!";
-            $scope.correct++;
+    //input: goals, shooting percentage
+    //output: shots
+    function getShots(goals, shootingP){
+        if(shootingP===0){
+            return 0;
         }
         else{
-            $scope.feedback = "INCORRECT.  " + $scope.player.name + " scored " + g + " goals.";
+            var shots = (100*goals) / shootingP;
+            shots = parseFloat(shots.toPrecision(4));
+            return Math.ceil(shots);
         }
-    }
-    else if($scope.unknownVar === 1){
-        var s = getShots($scope.player.goals, $scope.player.shootingPercentage);
-        if($scope.userEquiv===s){
-           $scope.feedback = "CORRECT!";
-           $scope.correct++; 
+    };
+
+    //input: goals, shots
+    //output: shooting percentage
+    function getShootingP(goals, shots){
+        if(shots===0){
+            var zero = 0.000
+            return zero.toPrecision(5);
         }
         else{
-            $scope.feedback = "INCORRECT.  " + $scope.player.name + " had " + s + " shots on goal.";
-        } 
+            var shootingP = (goals / shots)*100;
+            shootingP = parseFloat(shootingP.toPrecision(5));
+            return shootingP;
+        }
+    };    
+    
+$scope.checkUserInput = function(){
+    $scope.attempts++;
+    var calcVal;
+    var output;
+        if($scope.unknownVar===0){
+            calcVal = $scope.temp;
+            output = " goals scored ";
+            $scope.player.goals = $scope.temp;
+        }
+        else if($scope.unknownVar===1){
+            calcVal = $scope.temp;
+            output = " shots on goal ";
+            $scope.player.shots = $scope.temp;
+        }
+        else{
+            calcVal = getShootingP($scope.player.goals, $scope.player.shots);  
+            output = " shooting percentage ";
+            $scope.player.shootingPercentage = calcVal;
+        }
+    if($scope.userInput===calcVal){
+        $scope.feedback = "CORRECT!";
+        $scope.correct++;        
     }
     else{
-        var sp = getShootingP($scope.player.goals, $scope.player.shots);
-        if($scope.userEquiv===sp){
-           $scope.feedback = "CORRECT!";
-           $scope.correct++;    
-        }
-        else{
-            $scope.feedback = "INCORRECT.  " + $scope.player.name + " had a " + sp + " shooting percentage.";
-        } 
+        $scope.feedback = "INCORRECT.  " + $scope.player.name + "'s actual" + output + " has been filled-in above.";    
     }
-}    
+    $scope.disableCorrect = true;
+}
+        
+loadScenario();    
     
 });
